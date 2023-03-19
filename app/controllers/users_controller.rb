8f70072fa373f592ad1a141a63fd1d7826f1ead4
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-03-19 18:03:42 +0300

    Lab Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+
+    def show
+        users = User.find_by(id: params[:id])
+        render json: users,include: :items, status: :ok
+    # rescue ActiveRecord::RecordNotFound, with: not_found_response
+    end
 end
+
+# private
+# def not_found_response
+#     render json: {error: "User not found"}, status: :not_found
+# end
